3d9847974e01850c6076fc42659e1078179cac26
Ubuntu <ubuntu@ip-172-31-55-104.ec2.internal>
2020-12-03 12:42:50 +0000

    edit conflicts

--- project/app.js
+++ project/app.js
@@ -178,6 +178,95 @@ function today_gu(replyToken, message){ //오늘의 서울시 __구 확진자 �
 
 }
 
+function places(replyToken, message) {
+    console.log(message.text);
+    resultmessage = "";
+        (async() => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto("https://www.seoul.go.kr/coronaV/coronaStatus.do?menu_code=01");
+  
+            // 페이지의 HTML을 가져온다.
+            const content = await page.content();
+            // $에 cheerio를 로드한다.
+            const $ = cheerio.load(content);
+            var my= "";// 내 구
+            var all_gu = ["종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "중랑구", "성북구", "강북구", "도봉구", "노원구", "은평구", "서대문구", "마포구", "양천구", "강서구", "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구", "강동구"];
+            for (var i = 0; i<all_gu.length; i++){
+                if (message.text.indexOf(all_gu[i])!=-1){
+                    my = all_gu[i];
+                    break;
+                }
+            }
+            console.log(my);
+            var cnt = 0;
+            var ttr_lists = $("#DataTables_Table_0").children("tbody").children("tr");
+            for (var t = 0; t<ttr_lists.length; t++){
+                var tr_lists = ttr_lists.eq(t).children("td").children("table").children("tbody").children("tr");
+                        
+                scrapingResult = {//결과 나타낼 형식
+                        'district': '',
+                        'type': '',
+                        'business_name': '',
+                        'address': '',
+                        'date_time':'',
+                        'disinfection':''
+                    }
+                for (var row = 0; row<tr_lists.length; row++){
+                    scrapingResult['district'] = String(tr_lists.eq(row).find('td:nth-child(1)').text());
+                    if (scrapingResult['district']!==my) continue;//구역이 내 구역인 경우만
+                    scrapingResult['type'] = String(tr_lists.eq(row).find('td:nth-child(2)').text());
+                    if (scrapingResult['type']=="자택") continue;//자택 장소 필요X
+                    scrapingResult['business_name'] = String(tr_lists.eq(row).find('td:nth-child(3)').text());
+                    if (scrapingResult['business_name'].indexOf("비공개")!=-1) continue;//다 처리되어서 비공개인 장소들 처리
+                    if (scrapingResult['business_name'].indexOf("완료")!=-1) continue;
+                    scrapingResult['address'] = String(tr_lists.eq(row).find('td:nth-child(4)').text());
+                    if (scrapingResult['address'].indexOf("비공개")!=-1) continue;
+                    scrapingResult['date_time'] = String(tr_lists.eq(row).find('td:nth-child(5)').text());
+                    if (scrapingResult['date_time'].indexOf("비공개")!=-1) continue;
+                    if (scrapingResult['date_time'].indexOf("자가격리")!=-1) continue;//자가격리 환자 경로 처리
+                    scrapingResult['disinfection'] = String(tr_lists.eq(row).find('td:nth-child(6)').text());
+                    resultmessage+=scrapingResult['district']+" ";
+                    resultmessage+=scrapingResult['type']+" ";
+                    resultmessage+=scrapingResult['business_name']+" ";
+                    resultmessage+=scrapingResult['address']+" ";
+                    resultmessage+=scrapingResult['date_time']+"\n\n";
+                    console.log(scrapingResult);
+                    cnt++;
+                    if (cnt==10) break;
+                }
+                if (cnt==10) break;
+                
+            }
+  
+            if (cnt==0){
+            resultmessage+="확진자 방문 장소가 없습니다.\n\n";
+            }
+            resultmessage+="*「확진환자의 이동경로 등 정보공개 지침(1판)」에 따라 확진환자의 성별, 연령, 국적, 거주지(읍면동 단위 이하) 등 개인을 특정하는 정보는 공개되지 않습니다.*\n"
+            browser.close();
+                
+            request.post(
+                {
+                    url: TARGET_URL,
+                    headers: {
+                        'Authorization': `Bearer ${TOKEN}`
+                    },
+                    json: {
+                        "replyToken":replyToken,
+                        "messages":[
+                            {
+                                "type":"text",
+                                "text":resultmessage
+                            }
+                        ]
+                    }
+                },(error, response, body) => {
+                    console.log(body);
+                });
+        })();
+  
+    }
+
 function hospital_information(replyToken, keyword){ //__구 선별 진료소
 
   request.get(
@@ -266,94 +355,6 @@ function information(replyToken){ //서울시 코로나 정보봇 사용 매뉴�
 
 }
 
-function places(replyToken, message) {
-  console.log(message.text);
-  resultmessage = "";
-      (async() => {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-          await page.goto("https://www.seoul.go.kr/coronaV/coronaStatus.do?menu_code=01");
-
-          // 페이지의 HTML을 가져온다.
-          const content = await page.content();
-          // $에 cheerio를 로드한다.
-          const $ = cheerio.load(content);
-          var my= "";// 내 구
-          var all_gu = ["종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "중랑구", "성북구", "강북구", "도봉구", "노원구", "은평구", "서대문구", "마포구", "양천구", "강서구", "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구", "강동구"];
-          for (var i = 0; i<all_gu.length; i++){
-              if (message.text.indexOf(all_gu[i])!=-1){
-                  my = all_gu[i];
-                  break;
-              }
-          }
-          console.log(my);
-          var cnt = 0;
-          var ttr_lists = $("#DataTables_Table_0").children("tbody").children("tr");
-          for (var t = 0; t<ttr_lists.length; t++){
-              var tr_lists = ttr_lists.eq(t).children("td").children("table").children("tbody").children("tr");
-                      
-              scrapingResult = {//결과 나타낼 형식
-                      'district': '',
-                      'type': '',
-                      'business_name': '',
-                      'address': '',
-                      'date_time':'',
-                      'disinfection':''
-                  }
-              for (var row = 0; row<tr_lists.length; row++){
-                  scrapingResult['district'] = String(tr_lists.eq(row).find('td:nth-child(1)').text());
-                  if (scrapingResult['district']!==my) continue;//구역이 내 구역인 경우만
-                  scrapingResult['type'] = String(tr_lists.eq(row).find('td:nth-child(2)').text());
-                  if (scrapingResult['type']=="자택") continue;//자택 장소 필요X
-                  scrapingResult['business_name'] = String(tr_lists.eq(row).find('td:nth-child(3)').text());
-                  if (scrapingResult['business_name'].indexOf("비공개")!=-1) continue;//다 처리되어서 비공개인 장소들 처리
-                  if (scrapingResult['business_name'].indexOf("완료")!=-1) continue;
-                  scrapingResult['address'] = String(tr_lists.eq(row).find('td:nth-child(4)').text());
-                  if (scrapingResult['address'].indexOf("비공개")!=-1) continue;
-                  scrapingResult['date_time'] = String(tr_lists.eq(row).find('td:nth-child(5)').text());
-                  if (scrapingResult['date_time'].indexOf("비공개")!=-1) continue;
-                  if (scrapingResult['date_time'].indexOf("자가격리")!=-1) continue;//자가격리 환자 경로 처리
-                  scrapingResult['disinfection'] = String(tr_lists.eq(row).find('td:nth-child(6)').text());
-                  resultmessage+=scrapingResult['district']+" ";
-                  resultmessage+=scrapingResult['type']+" ";
-                  resultmessage+=scrapingResult['business_name']+" ";
-                  resultmessage+=scrapingResult['address']+" ";
-                  resultmessage+=scrapingResult['date_time']+"\n\n";
-                  console.log(scrapingResult);
-                  cnt++;
-                  if (cnt==10) break;
-              }
-              if (cnt==10) break;
-              
-          }
-
-          if (cnt==0){
-          resultmessage+="확진자 방문 장소가 없습니다.\n\n";
-          }
-          resultmessage+="*「확진환자의 이동경로 등 정보공개 지침(1판)」에 따라 확진환자의 성별, 연령, 국적, 거주지(읍면동 단위 이하) 등 개인을 특정하는 정보는 공개되지 않습니다.*\n"
-          browser.close();
-              
-          request.post(
-              {
-                  url: TARGET_URL,
-                  headers: {
-                      'Authorization': `Bearer ${TOKEN}`
-                  },
-                  json: {
-                      "replyToken":replyToken,
-                      "messages":[
-                          {
-                              "type":"text",
-                              "text":resultmessage
-                          }
-                      ]
-                  }
-              },(error, response, body) => {
-                  console.log(body);
-              });
-      })();
-
-  }
 function steps(replyToken, message){
       console.log("steps");
       const workbook = xlsx.readFile("stepexcel.xlsx");//거리두기 정보 담긴 엑셀 읽기
