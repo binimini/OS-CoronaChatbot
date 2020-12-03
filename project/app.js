c184825f3de5b0c61877e59041ee0ba248639e0f
Ubuntu <ubuntu@ip-172-31-55-104.ec2.internal>
2020-12-03 11:15:10 +0000

    3번 구현 완료

--- project/app.js
+++ project/app.js
@@ -9,7 +9,9 @@ client.set('headers', { // 크롤링 방지 우회를 위한 User-Agent setting
 var express = require('express');
 const request = require('request');
 const TARGET_URL = 'https://api.line.me/v2/bot/message/reply'
+const MAP_URL = 'https://dapi.kakao.com/v2/local/search/keyword.json'
 const TOKEN = 'r/qgCfP0wwGegeaGmAvPTztE0nCDg5t35IUJap+U2i0Kvm0DMMjxdiAPQ/Pg+zAqaJrMh8c1Oj/QtGZTBOwgKLmQrT3xkAyCA26ipxYPmMwbjg7C6JhxeGI7TEyBXDP2qKmACxledtL8zzqRMOlLvAdB04t89/1O/w1cDnyilFU=';
+const KAKAO_ID = 'fb1345dd38817291d1f9105a21488b17';
 const fs = require('fs');
 const path = require('path');
 const HTTPS = require('https');
@@ -41,6 +43,10 @@ app.post('/hook', function (req, res) {
     }
     else if(all_gu.indexOf(message.text) !== -1){ //1-1 오늘의 서울시 __구 확진자 수
       today_gu(eventObj.replyToken, eventObj.message.text);
+    }
+    else if(message.text.indexOf("선별진료소") !== -1 || message.text.indexOf("선별 진료소") !== -1){
+      // 3. __구 선별진료소
+      hospital_information(eventObj.replyToken, eventObj.message.text);
     }
     else{ //매뉴얼 이외의 입력은 모두 도움말 출력
       information(eventObj.replyToken);
@@ -164,6 +170,66 @@ function today_gu(replyToken, message){ //오늘의 서울시 __구 확진자 �
 
 }
 
+function hospital_information(replyToken, keyword){ //__구 선별 진료소
+
+  request.get(
+    {
+      url: MAP_URL,
+      headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': `KakaoAK ${KAKAO_ID}`
+      },
+      form : {
+          'query' : keyword,
+          'size' : 3
+      },
+      json : true
+    }, (error,response,body) => {
+        if(!error && response.statusCode == 200) {
+          console.log(body.documents);
+          var result_1 = body.documents[0];
+          var result_2 = body.documents[1];
+          var result_3 = body.documents[2];
+          request.post(
+            {
+                url: TARGET_URL,
+                headers: {
+                    'Authorization': `Bearer ${TOKEN}`
+                },
+                json: {
+                    "replyToken":replyToken,
+                    "messages":[
+                        {
+                          "type": "location",
+                          "title": result_1.place_name,
+                          "address": result_1.road_address_name,
+                          "latitude":  result_1.y,
+                          "longitude": result_1.x
+                        },
+                        {
+                          "type": "location",
+                          "title": result_2.place_name,
+                          "address": result_2.road_address_name,
+                          "latitude":  result_2.y,
+                          "longitude": result_2.x
+                        },
+                        {
+                          "type": "location",
+                          "title": result_3.place_name,
+                          "address": result_3.road_address_name,
+                          "latitude":  result_3.y,
+                          "longitude": result_3.x
+                        }
+                    ]
+                }
+            },(error, response, body) => {
+                console.log(body)
+            });
+    }
+  });
+  
+  }
+
 function information(replyToken){ //서울시 코로나 정보봇 사용 매뉴얼
 
   request.post(
@@ -181,7 +247,7 @@ function information(replyToken){ //서울시 코로나 정보봇 사용 매뉴�
                 },
                 {
                     "type":"text",
-                    "text":"서울시 코로나 정보봇 사용 방법\n 1. 오늘의 서울시 확진자 정보가 궁금하다면 '오늘 확진자'를 입력해주세요!\n 2. 확진자 발생 장소 관련 공지를 보고 싶으시다면 '공지'를 입력해주세요! \n 3. 코로나 선별진료소 정보가 궁금하다면 '__구 선별진료소'를 입력해주세요! ex) 종로구 선별진료소 \n 4. 거리두기 단계별 안전 수칙 및 정보가 궁금하다면 '거리두기'를 입력해주세요! "
+                    "text":"서울시 코로나 정보봇 사용 방법\n\n 1. 오늘의 서울시 확진자 정보가 궁금하다면 '오늘 확진자'를 입력해주세요!\n\n 2. 확진자 발생 장소 관련 공지를 보고 싶으시다면 '공지'를 입력해주세요! \n\n 3. 코로나 선별진료소 정보가 궁금하다면 '__구 선별진료소'를 입력해주세요! ex) 종로구 선별진료소 \n\n 4. 거리두기 단계별 안전 수칙 및 정보가 궁금하다면 '거리두기'를 입력해주세요! "
                     //매뉴얼이므로 수정하기!
                 }
             ]
